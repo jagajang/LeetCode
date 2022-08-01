fa9f48609f76c798dfc43f437e0a6817dad47818
jagajang <kosb7544@gmail.com>
2022-08-01 12:09:00 +0900

    Time: 3 ms (44.81%), Space: 6 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // 아래 m-1번, 오른쪽 n-1번
         long long ret = 1;
-        for(int i = 1; i < min(n, m); i++) {
-            ret *= n + m - i - 1;
+        long long su = n + m - 1;
+        long long mi = min(n, m);
+        
+        for(long long i = 1; i < mi; i++) {
+            ret *= su - i;
             ret /= i;
         }
             

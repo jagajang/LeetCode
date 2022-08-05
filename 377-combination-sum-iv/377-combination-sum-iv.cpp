cf70e4d3ba0e16c080f2468377f30fd75ceea38a
jagajang <kosb7544@gmail.com>
2022-08-05 12:07:17 +0900

    Time: 9 ms (10.81%), Space: 6.8 MB (30.10%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    const int INT_OVERFLOW = 10000000000;
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector < long long > numCase;
+        
+        numCase.resize(target+1);
+        sort(nums.begin(), nums.end());
+        
+        numCase[0] = 1;
+        
+        for(int i = 0; i < target; i++)
+            for(int j = 0; j < nums.size(); j++) {
+                int next = i + nums[j];
+                if(next > target)
+                    break;
+                    
+                numCase[next] += numCase[i];
+                numCase[next] %= INT_OVERFLOW;
+            }
+                
+        
+        return numCase[target];
+    }
+};

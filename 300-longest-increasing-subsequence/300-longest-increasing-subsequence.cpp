151d0c1fdfe81943359af19087b5a1f3706214e2
jagajang <kosb7544@gmail.com>
2022-08-08 13:19:44 +0900

    Time: 9 ms (94.29%), Space: 10.3 MB (97.96%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
+        int ret = 1;
         vector < int > lenMin;
         
         lenMin.push_back(nums[0]);
@@ -11,6 +11,7 @@ public:
             
             if(lenMin.back() < num) {
                 lenMin.push_back(num);
+                ret++;
             }
             else {
                 auto iter = lower_bound(lenMin.begin(), lenMin.end(), num);
@@ -18,6 +19,6 @@ public:
             }
         }
         
-        return lenMin.size();
+        return ret;
     }
 };

0aaa8b3bd5c0e778092cbd2646aa51e255aae1a6
jagajang <kosb7544@gmail.com>
2022-08-17 10:15:08 +0900

    Time: 7 ms (46.48%), Space: 8.4 MB (88.09%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    const string morse[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    
+    string wordToMorse(string word) {
+        string ret = "";
+        
+        for(size_t i = 0; i < word.size(); i++) {
+            ret += morse[word[i] - 'a'];
+        }
+        
+        return ret;
+    }
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        set <string > morse;
+        
+        for(size_t i = 0; i < words.size(); i++) {
+            morse.insert(wordToMorse(words[i]));
+        }
+        
+        return morse.size();
+    }
+};

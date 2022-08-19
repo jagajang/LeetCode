138244dff14ab8260e262520baf0aa26f8c143e1
jagajang <kosb7544@gmail.com>
2022-08-19 12:49:43 +0900

    Time: 116 ms (84.42%), Space: 54.7 MB (81.35%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool isPossible(vector<int>& nums) {
         
-        //cout << "--- tc start ---\n";
-        
         queue < int > seqStart;
         int numsIndex = 0;
         
@@ -13,30 +11,20 @@ public:
             
             for(; numsIndex < nums.size() && nums[numsIndex] == val; numsIndex++)
                 count++;
-            
-            /*if(count)
-                cout << val << " : " << seqStart.size() << ", " << count << "\n";
-            */
+
             if(count < seqStart.size()) {
-                // drop sequence
                 for(int i = 0; seqStart.size() > count; i++) {
                     int fr = seqStart.front();
                     seqStart.pop();
-                    
-                    //cout << "drop " << fr << "\n";
                     
                     if(fr + 3 > val)
                         return false;
                 }
             }
             else if(count > seqStart.size()) {
-                // add sequence
-                for(int i = 0; seqStart.size() < count; i++) {
-                    //cout << "add " << val << "\n";
+                for(int i = 0; seqStart.size() < count; i++)
                     seqStart.push(val);
-                }
             }
-            // numMap[val] == numMap.size() => do nothing
         }
         
         return true;
